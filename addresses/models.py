fda27e49e30f860badbab9b138311fa9fb93fe98
Lucas Samuel F. S <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2023-05-03 16:08:05 -0300

    feat[app address]: create model and serializer

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=50)
+    number = models.CharField(max_length=8)
+    complement = models.TextField(null=True)
+    district = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        "users.User",
+        related_name="Addresses",
+        on_delete=models.CASCADE,
+    )
---
+++ addresses/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from addresses.models import Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = [
+            "id",
+            "street",
+            "number",
+            "complement",
+            "district",
+            "user",
+        ]
+        read_only_fields = ["complement"]
+
+    def create(self, validated_data: dict) -> Address:
+        return Address.objects.create(**validated_data)
