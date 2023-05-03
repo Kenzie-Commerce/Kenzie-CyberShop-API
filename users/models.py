0ef30998ada140154f4de9cf88548b9036043168
Felippe Gomes <66650192+lppgomes@users.noreply.github.com>
2023-05-03 15:42:54 -0300

    feat: users model and serializer

--- users/models.py
+++ users/models.py
@@ -8,7 +8,6 @@ class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
 
     email = models.EmailField(max_length=127, unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
 
+    is_superuser = models.BooleanField(default=False, null=True)
     is_seller = models.BooleanField(default=False, null=True)
