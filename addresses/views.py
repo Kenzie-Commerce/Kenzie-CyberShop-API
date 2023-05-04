adc70669b84c71bb5a417d9c87865ea2e232ce29
Lucas Samuel F. S <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2023-05-04 11:12:55 -0300

    feat[address app]: create views address

--- addresses/views.py
+++ addresses/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from addresses.models import Address
+from addresses.serializers import AddressSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from addresses.permissions import IsOwnerAddress
+
 
 # Create your views here.
+class AddressView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
+
+
+class AddressDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwnerAddress]
+    lookup_url_kwarg = "address_id"
+
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
