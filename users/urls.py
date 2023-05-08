974c7fbab5fa1d3dbc3361efd1407eb2a140d6c3
Mayza <mayza.santos@outlook.com.br>
2023-05-07 23:37:31 -0300

    feat: views user

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
+from users.views import UserView, UserDetailView
 from addresses.views import AddressView, AddressDetailView
 from carts.views import CartListView
 
 
 urlpatterns = [
-    # path("users/"),
-    # path("users/<int:id_user>/"),
+    path("users/", UserView.as_view()),
+    path("users/<int:id_user>/", UserDetailView.as_view()),
     path("users/login/", TokenObtainPairView.as_view()),
     path("users/login/refresh/", TokenObtainPairView.as_view()),
     path("users/address/", AddressView.as_view()),
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from users.models import User
+from users.serializers import UserSerializer
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+class UserView(CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
